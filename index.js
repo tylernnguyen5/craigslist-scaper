02bbfe00a5637aaca637af9b894396a096ee9244
Tyler <tyler.nnguyen5@gmail.com>
2020-07-10 17:15:35 +1000

    Implemented scraping list of job from Craigslist SfBay

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
 
 async function main() {
-    let url = "https://www.google.com";
+    let url = "https://sfbay.craigslist.org/d/software-qa-dba-etc/search/sof";
     
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto(url);
 
+    const html = page.content();
+
+    const $ = cheerio.load(html);
+
+    // Object
+    const results = $(".result-info")
+        .map( (index, element) => {
+            const titleElement = $(element).find(".result-title");
+            const timeElement = $(element).find(".result-date");
+
+            const title = $(titleElement).text();
+            const url = $(titleElement).attr("href");
+            const datePosted = new Date($(timeElement).attr("datetime"));
+            const neighborhood = $(element).find(".result-hood").text()
+                .trim()
+                .replace("(", "")
+                .replace(")", "");
+
+            return { title, url, datePosted, neighborhood };
+        })
+        .get();
+
+    console.log(results);
 }
 
 main();
