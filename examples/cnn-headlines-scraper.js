df1a404b7f75953c484a757f834bb9cd8c7b1490
Tyler <tyler.nnguyen5@gmail.com>
2020-07-15 15:37:36 +1000

    Implemented scraping url for cnn scraper example

--- examples/cnn-headlines-scraper.js
+++ examples/cnn-headlines-scraper.js
@@ -4,27 +4,30 @@ const cheerio = require('cheerio');
 
 // Main function
 async function main() {
-		let url = "https://www.cnn.com/";
+	let url = "https://www.cnn.com";
 
-		const browser = await puppeteer.launch();
+	const browser = await puppeteer.launch();
 
-		const page = await browser.newPage();
+	const page = await browser.newPage();
 
-		await page.goto(url, {
-			waitUntil: 'networkidle2'
-		});
-		
-		const html = await page.content();
-		const $ = cheerio.load(html);
+	await page.goto(url, {
+		waitUntil: 'networkidle2'
+	});
+	
+	const html = await page.content();
+	const $ = cheerio.load(html);
 
-		// Scrape
-		$("section#intl_homepage1-zone-1 > div > div > div > ul > li > article > div > div > h3 > a > span.cd__headline-text").each( (index, element) => {
-			headline = $(element).text();
+	// Scrape
+	$("section#intl_homepage1-zone-1 > div > div > div > ul > li > article > div > div > h3 > a").each( (index, element) => {
+		let href = $(element).attr("href");
+		let headline = $(element).find("span.cd__headline-text").text();
 
-			console.log(headline);
-		});
+		console.log(`Headline: ${headline}`);
+		console.log(`URL: ${url}${href}`);
+		console.log("\n");
+	});
 
-		await browser.close()
+	await browser.close()
 }
 
 main();
